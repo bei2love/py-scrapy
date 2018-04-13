5a191980e2917953bfb00fcd7f90f3c9a3032bb6
丁先生 <thinkercloud@dingxiaengdeMBP.padavan>
2018-04-13 18:19:40 +0800

    add thread support

--- sex/pipelines.py
+++ sex/pipelines.py
@@ -17,6 +17,7 @@ import re
 import urllib
 import urllib2
 import os
+import threading
 
 class SexPipeline(object):
 
@@ -53,14 +54,29 @@ class SexPipeline(object):
         #     print('Error %d %s' %(e.args[0], e.args[1]))
         line = "%s \t%s\n"%(item['title'], item['url'])
         # self.file.write(line)
-        path = '/Users/thinkercloud/Desktop/aaa/';
-        folder = path + item['title'].strip()
+        download = downloadTread(item)
+        download.start();
+
+        return item
+
+
+
+
+
+class downloadTread(threading.Thread):
+    def __init__(self, item):
+        threading.Thread.__init__(self)
+        self.item = item
+
+    def run(self):
+        path = '/Users/xxx/Desktop/aaa/';
+        folder = path + self.item['title'].strip()
 
         if not os.path.exists(folder):
             os.mkdir(folder)
 
-        for img in item['image_urls']:
-            filename = re.findall(r'/\d+\.[a-z]*',img.encode('utf-8'))
+        for img in self.item['image_urls']:
+            filename = re.findall(r'/\d+\.[a-z]*', img.encode('utf-8'))
 
             hdr = {
                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
@@ -78,5 +94,3 @@ class SexPipeline(object):
                 f.close()
             except urllib2.HTTPError, e:
                 print e.fp.read()
-        return item
-
